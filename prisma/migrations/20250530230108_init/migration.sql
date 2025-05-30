3b4c0c0233de002c09c169f7e455c30857fd0b43
Paulo Henrique Chi <paulo.henrique.chi@outlook.com>
2025-05-30 20:17:41 -0300

    feat: add prisma

---
+++ prisma/migrations/20250530230108_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Book" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "author" TEXT NOT NULL,
+    "finished" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
